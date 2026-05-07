0270e5920db3cae5dc700d8d4ce4cea47a32d383
Diogo Melo <98532975+Digao46@users.noreply.github.com>
2026-05-07 14:02:34 -0300

    chore: angular material and cdk to version 15.x.x

--- projects/kf-easy-helpers/src/lib/controller/controller.component.ts
+++ projects/kf-easy-helpers/src/lib/controller/controller.component.ts
@@ -1,4 +1,4 @@
-import { MatTableDataSource } from '@angular/material/table';
+import { MatLegacyTableDataSource as MatTableDataSource } from '@angular/material/legacy-table';
 import { Component } from '@angular/core';
 import { HelperService } from '../services/helper.service';
 import { BehaviorSubject } from 'rxjs';
