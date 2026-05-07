165556f0ac3cd9476388128f4058b8ea35db92ef
Diogo Melo <98532975+Digao46@users.noreply.github.com>
2026-05-07 13:46:21 -0300

    chore: upgrading core and cli to version 13.x.x

--- projects/kf-easy-helpers/src/public-api.ts
+++ projects/kf-easy-helpers/src/public-api.ts
@@ -1,22 +1,24 @@
 /*
  * Public API Surface of kf-easy-helpers
  */
+export * from "./lib/kf-easy-helpers.component";
+export * from "./lib/kf-easy-helpers.module";
 
-export * from './lib/kf-easy-helpers.component';
-export * from './lib/kf-easy-helpers.module';
-export * from './lib/controller/controller.component'
-export * from './lib/services/helper.service'
-export * from './lib/services/model-rest.service'
-export * from './lib/services/request.service'
-export * from './lib/kf-acl/kf-acl.service';
-export * from './lib/kf-acl/kf-acl-guard.service';
+export * from "./lib/controller/controller.component";
 
+export * from "./lib/services/helper.service";
+export * from "./lib/services/model-rest.service";
+export * from "./lib/services/request.service";
 
-export * from './lib/kf-env-config';
-export * from './lib/kf-env-type';
+export * from "./lib/kf-acl/kf-acl.service";
+export * from "./lib/kf-acl/kf-acl-guard.service";
+export * from "./lib/kf-acl/kf-acl.component";
 
+export * from "./lib/kf-env-config";
+export * from "./lib/kf-env-type";
 
-// components
-export * from './lib/filter/filter.component';
-export * from './lib/loading/loading.component';
-export * from './lib/status/status.component';
+export * from "./lib/filter/filter.component";
+
+export * from "./lib/loading/loading.component";
+
+export * from "./lib/status/status.component";
